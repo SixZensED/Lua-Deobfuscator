ce359f39ad1b74e3b12ff2ba9dae4cbec51a9de3
SixZens <106856251+SixZensED@users.noreply.github.com>
2023-11-01 14:50:30 +0700

    Update deobfuscate.js

--- obfuscators/moonsec/deobfuscate.js
+++ obfuscators/moonsec/deobfuscate.js
@@ -1,1 +1,27 @@
+const chalk = require("chalk")
+const analyze = require("./analyze")
+const deserialize = require("./deserialize")
+const devirtualize = require("./devirtualize")
+const binary = require("../../utility/binary")
+const printer = require("../../utility/printer")
 
+function detect(source) {
+    return source.match(/\.\.:::MoonSec::\.\./g) != null
+}
+
+function deobfuscate(ast) {
+    let vmdata
+
+    process.stdout.write(`[${chalk.blueBright("INFO")}] Finding Vm Data...`)
+    vmdata = analyze(ast.StatementList, true)
+
+    process.stdout.write(`[${chalk.blueBright("INFO")}] Deserializing Bytecode...`)
+    vmdata = deserialize(vmdata, true)
+
+    process.stdout.write(`[${chalk.blueBright("INFO")}] Devirtualizing Bytecode...`)
+    vmdata = devirtualize(vmdata, true)
+
+    return binary.sterilize(vmdata)
+}
+
+module.exports = {detect, deobfuscate, name: "MoonSecV2"}
